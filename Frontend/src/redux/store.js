428f72aeecd9cf1b653505d6a706c63438d5382c
xainmuhammadkhan <155022654+XainMuhammadKhan@users.noreply.github.com>
2026-02-07 17:37:07 +0500

    Update store.js

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./userSlice.js";
 import courseSlice from "./courseSlice.js";
-import lectureSlice from "./LectureSlice.js";
+import lectureSlice from "./lectureSlice.js";
 import reviewSlice from "./reviewSlice.js";
 export const store = configureStore({
     reducer: {
